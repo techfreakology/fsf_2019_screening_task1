561f2f5aa353235487c888a87d0fae398aabd056
vivekcrux <techfreakology@gmail.com>
2019-03-17 19:42:11 +0530

    added login/logout functionality for authorizations of users

--- task_manager/task_manager/urls.py
+++ task_manager/task_manager/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$',views.HomePage.as_view(),name="home"),
+    url(r'^accounts/',include('accounts.urls',namespace='accounts')),
+    url(r'^accounts/',include('django.contrib.auth.urls')),
+    url(r'^test/$',views.TestPage.as_view(),name='test'),
+    url(r'^thanks/$',views.ThanksPage.as_view(),name='thanks'),
 ]
