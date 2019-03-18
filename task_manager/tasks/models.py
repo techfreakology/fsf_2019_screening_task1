21a6d82c92e943a99e68ba25db87f4fd97667950
vivekcrux <techfreakology@gmail.com>
2019-03-19 00:21:30 +0530

    Added Task, TaskAssignees, Team and TeamMember  models

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.urls import reverse
+from django.conf import settings
+from django.utils.text import slugify
 
+from teams.models import Team
+from django.contrib.auth import get_user_model
+User = get_user_model()
 # Create your models here.
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255,unique=True)
+    description = models.TextField(blank=True,default='')
+
+    STATUS_CHOICES = (
+        ("PLANNED", "Planned"),
+        ("INPROGRESS", "Inprogress"),
+        ("DONE", "Done"),
+    )
+    status = models.CharField(max_length=10,
+                              choices=STATUS_CHOICES,
+                              default="PLANNED")
+
+    slug = models.SlugField(allow_unicode=True,unique=True)
+    creater = models.ForeignKey(User,related_name="tasks",on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    assignees = models.ManyToManyField(User,through="TaskAssignees")
+
+    def __str__(self):
+        return self.title
+
+    def save(self,*args,**kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args,**kwargs)
+
+    def get_absolute_url(self):
+        return reverse("tasks:single",kwargs={'slug':self.slug,'username':self.creater.username})
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class TaskAssignees(models.Model):
+    task = models.ForeignKey(Task,related_name="assigned",on_delete=models.CASCADE)
+    user = models.ForeignKey(User,related_name="user_tasks",on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        unique_together = ("task","user")
--- task_manager/teams/models.py
+++ task_manager/teams/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.utils.text import slugify
+from django.urls import reverse
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+from django import template
+register = template.Library()
 # Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=255,unique=True)
+    slug = models.SlugField(allow_unicode=True,unique=True)
+    description = models.TextField(blank=True,default='')
+
+    def __str__(self):
+        return self.name
+
+    def save(self,*args,**kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args,**kwargs)
+
+    def get_absolute_url(self):
+        return reverse('teams:single',kwargs={'slug':self.slug})
+
+class TeamMember(models.Model):
+    team = models.ForeignKey(Team,related_name="memberships",on_delete=models.CASCADE)
+    user = models.ForeignKey(User,related_name="user_team",on_delete=models.CASCADE)
+
+    MEMBERSHIP_CHOICES = (
+            ("LEADER", "Leader"),
+            ("MEMBER", "Member"),
+    )
+    membership_type = models.CharField(max_length=6,
+                                       choices=MEMBERSHIP_CHOICES,
+                                       default="MEMBER")
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        unique_together = ("team","user")
